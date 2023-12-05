7804ec3b8eff0e9db652185b029841a651b179f3
Zackry Langford <zackrylangford@gmail.com>
2023-12-05 15:26:20 -0500

    initial commit

---
+++ .aws-sam/build/putItemFunction/app.js
@@ -0,0 +1,43 @@
+document.getElementById('getAllItems').addEventListener('click', function() {
+    fetch('https://2jkh43sq9k.execute-api.us-east-1.amazonaws.com/Prod/')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('items').innerText = JSON.stringify(data, null, 2);
+        });
+});
+
+document.getElementById('getItem').addEventListener('click', function() {
+    const id = document.getElementById('getItemId').value;
+    fetch(`https://2jkh43sq9k.execute-api.us-east-1.amazonaws.com/Prod/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('singleItem').innerText = JSON.stringify(data, null, 2);
+        });
+});
+
+document.getElementById('deleteItem').addEventListener('click', function() {
+    const id = document.getElementById('deleteItemId').value;
+    console.log("Delete this id please: " + id);
+    fetch(`https://2jkh43sq9k.execute-api.us-east-1.amazonaws.com/Prod/${id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => {
+            alert('Item deleted');
+        });
+});
+
+
+document.getElementById('addItem').addEventListener('click', function() {
+    const id = document.getElementById('newItemId').value;
+    const name = document.getElementById('newItemName').value;
+    fetch('https://2jkh43sq9k.execute-api.us-east-1.amazonaws.com/Prod/', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id, name })
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert('Item added');
+        });
+});
---
+++ src/handlers/get_by_id.py
@@ -0,0 +1,24 @@
+import json
+import os
+import boto3
+
+client = boto3.client('dynamodb')
+
+def getByIdHandler(event, context):
+    if event["httpMethod"] != "GET":
+        raise Exception(f"getByIdHandler only accept GET method, you tried: {event.httpMethod}")
+
+    id = event["pathParameters"]["id"]
+    data = client.get_item(TableName=os.environ["SAMPLE_TABLE"], Key={"id": {"S": id}})
+    item = data["Item"]
+    response = {
+        "statusCode": 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true',
+        },
+        "body": json.dumps(item)
+    }
+
+    return response
